c0360f6313919e326ce5626bde138e03478980f7
Sonila Kar <skar4@asu.edu>
2020-06-23 17:57:28 -0700

    small dataset update

--- apriori.R
+++ apriori.R
@@ -7,15 +7,19 @@ retail_data <- read.csv("Online Retail.csv")
 
 retail_data_subset <- retail_data[,c(1,3)]
 
-#retail_data_subset <- retail_data_subset %>% top_n(10000)
-
 retail_data_subset <- filter(retail_data_subset, Description != "")
+
+#retail_data_subset <- sample_n(retail_data_subset, 10000)
 
 retail_data_subset$prod_count <- 1
 
 retail_data_subset2 <- retail_data_subset %>%
   group_by(InvoiceNo, Description) %>%
   summarise(prod_count = sum(prod_count))
+
+#retail_data_subset3 <- retail_data_subset %>%
+#  group_by(Description) %>%
+#  summarise(n())
 
 data_wide <- spread(retail_data_subset2, Description, prod_count)
 
@@ -28,10 +32,24 @@ col_names <- names(data_wide)
 data_wide[,col_names] <- lapply(data_wide[,col_names] , factor)
 
 rules <- apriori(data_wide, 
-                 parameter = list(minlen = 2,
-                 maxlen = 3,
-                 supp = 0.9))
+                 #parameter = list(minlen = 2,
+                 #maxlen = 3,
+                 #supp = 0.5),
+                 #conf=0.9))
+                 appearance = list(rhs=c("REGENCY CAKESTAND 3 TIER=1"),
+                                   default="lhs"))
 
 inspect(rules)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+retail_data <- read.csv("small retail data.csv")
+col_names <- names(retail_data)
+retail_data[,col_names] <- lapply(retail_data[,col_names] , factor)
+
+rules <- apriori(retail_data, 
+                 parameter = list(supp = 0.5),
+                 appearance = list(rhs=c("Bread=1"), default="lhs"))
+
+inspect(rules)
 
